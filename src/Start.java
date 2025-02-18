fc60880bce7c50079416b0ad060ee158aa3242b3
Rcepenko <rcepenkov@gmail.com>
2025-02-18 02:33:05 +0200

    &&

--- src/Start.java
+++ src/Start.java
@@ -4,11 +4,12 @@ public class Start {
 
     public static void main (String[] args) {
       int num1 = 7;
-      int num2 = 6;
-      int num3 = num2;
+      int num2 = 7;
+      int num3 = 7;
+      int num4 = 5;
 
       int result = num1 % num2;
-      if (num1 == num2) {
+      if (num1 == num2 && num3 > num4) {
           System.out.println("Результат равен");
       } else {
           System.out.println("Результат не равен");
