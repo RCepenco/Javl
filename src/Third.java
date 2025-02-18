feb2f1dfc3f88ae050cfb744264d7f437d1fa25c
Rcepenko <rcepenkov@gmail.com>
2025-02-18 04:14:38 +0200

    while

---
+++ src/Third.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+
+public class Third {
+
+    public static void main (String[] args) throws IOException {
+    int count = 0;
+
+        while (count < 10) {
+            System.out.println(count);
+            count++;
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
