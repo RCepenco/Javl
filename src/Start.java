b4b6895de4389e658006021458ba5d24da52781e
Rcepenko <rcepenkov@gmail.com>
2025-02-18 02:19:23 +0200

    4

--- src/Start.java
+++ src/Start.java
@@ -6,7 +6,7 @@ public class Start {
       int num1 = 7;
       int num2 = 3;
 
-      int result = num1 * num2;
+      int result = num1 % num2;
 
       System.out.println("Результат: " + result);
 
