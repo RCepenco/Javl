e9ce3add7c599fb35bcd0219e8b1514dcb56f028
Rcepenko <rcepenkov@gmail.com>
2025-02-18 04:06:05 +0200

    for (int i = 0; (char) System.in.read() != 'S'; i++)

--- src/Second.java
+++ src/Second.java
@@ -1,18 +1,15 @@
+import java.io.IOException;
+
 public class Second {
 
 
-    public static void main (String[] args) {
+    public static void main (String[] args) throws IOException  {
 
-        for (int i = 0; i < 5; i++){
+        for (int i = 0; (char) System.in.read() != 'S'; i++){
+
             System.out.println("Итерация" + i);
         }
 
-
-
-
     }
-
-
-
 
 }
