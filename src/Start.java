19bc4e81f1f7b480e69b6ae765f2d94786340292
Rcepenko <rcepenkov@gmail.com>
2025-02-18 02:18:01 +0200

    4

--- src/Start.java
+++ src/Start.java
@@ -3,10 +3,10 @@ public class Start {
 
 
     public static void main (String[] args) {
-      int num1 = 3;
-      int num2 = 4;
+      int num1 = 7;
+      int num2 = 3;
 
-      int result = num1 + num2;
+      int result = num1 - num2;
 
       System.out.println("Результат: " + result);
 
