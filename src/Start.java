a0a213009018bb2c13ec4aff81dff8306b806cc8
Rcepenko <rcepenkov@gmail.com>
2025-02-18 02:51:32 +0200

    if_else if

--- src/Start.java
+++ src/Start.java
@@ -1,23 +1,23 @@
+import java.io.IOException;
+
 public class Start {
 
 
+    public static void main (String[] args) throws IOException {
 
-    public static void main (String[] args) {
-      int num1 = 7;
-      int num2 = 6;
-      int num3 = 7;
-      int num4 = 5;
+       char ch, answer = 'B';
 
-      int result = num1 % num2;
-      if (num1 == num2 || num3 > num4) {
-          System.out.println("Результат равен");
-      } else {
-          System.out.println("Результат не равен");
-      }
+       System.out.println("Какую букву я загадал ?");
+       System.out.print("Попытаетесь ее отгадать: ");
+
+       ch = (char)System.in.read();
 
 
-
-
+       if (ch == answer) {
+           System.out.print("Поздравляю! ");
+       } else if (ch > answer) {
+           System.out.print("Перестарался ! ");
+       } else System.out.print("Недостарался ! ");
 
     }
 }
