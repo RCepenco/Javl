d739c20694af514b47984db1505de30772c763f0
Rcepenko <rcepenkov@gmail.com>
2025-02-18 02:27:49 +0200

    равен

--- src/Start.java
+++ src/Start.java
@@ -4,11 +4,17 @@ public class Start {
 
     public static void main (String[] args) {
       int num1 = 7;
-      int num2 = 3;
+      int num2 = 7;
+      int num3 = num2;
 
       int result = num1 % num2;
+      if (num1 == num2) {
+          System.out.println("Результат равен");
+      } else {
+          System.out.println("Результат не равен");
+      }
 
-      System.out.println("Результат: " + result);
+
 
 
 
