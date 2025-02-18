1a632b95429d2d3c03ee2d0500534b5b46b4e6b9
Rcepenko <rcepenkov@gmail.com>
2025-02-18 04:17:58 +0200

    do while

--- src/Third.java
+++ src/Third.java
@@ -3,12 +3,12 @@ import java.io.IOException;
 public class Third {
 
     public static void main (String[] args) throws IOException {
-    int count = 0;
+    int count = 10;
 
-        while (count < 10) {
+        do   {
             System.out.println(count);
             count++;
-        }
+        } while (count < 10);
 
     }
 
