94b8dcdb9c9b6ccdc2f78a0ac70bac82ababa4a6
Rcepenko <rcepenkov@gmail.com>
2025-02-19 16:15:54 +0200

    println_print

--- src/Max.java
+++ src/Max.java
@@ -1,11 +1,17 @@
-public class Max {
+public class Max
+    {
+        public static void main(String[] args)
+        {
 
-    public static void main (String[] args){
-        System.out.println(Integer.MAX_VALUE);
+            System.out.println("Amigo");
+            System.out.println("Is The");
+            System.out.println("Best");
 
+            System.out.print("Amigo ");
+            System.out.print("Is The ");
+            System.out.print("Best");
+
+
+        }
     }
 
-
-
-
-}
