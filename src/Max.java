d67d41f97341d8bb94041562d542b340d8a955be
Rcepenko <rcepenkov@gmail.com>
2025-02-19 22:50:18 +0200

    Comment out some code so that "sum = 12" appears on the screen.

--- src/Max.java
+++ src/Max.java
@@ -2,16 +2,16 @@ public class Max
      {
         public static void main(String[] args)
         {
-            // String s = "Merry";
-            // System.out.println("Christmas");
-            // System.out.println("Merry New");
-            // System.out.println("weekend");
-            System.out.print("Happy New");
-            // System.out.println("weekend");
-            // System.out.println(s);
-            // System.out.print("Merry New!");
-            System.out.print(" ");
-            System.out.println("Year");
+            int a = 3;
+            //int a = 10;
+            int b = 6;
+            //int b = 12;
+            //int sum = 1 + a + b;
+            //int sum = 2 + a + b;
+            int sum = 3 + a + b;
+            //int sum = 4 + a + b;
+
+            System.out.println("sum = " + sum);
 
 
     }
