b030370823944e3dba32fb58a4ed942faf2ff353
Rcepenko <rcepenkov@gmail.com>
2025-02-20 00:24:01 +0200

    создание переменной

--- src/Max.java
+++ src/Max.java
@@ -2,19 +2,13 @@ public class Max
      {
         public static void main(String[] args)
         {
-            int a = 3;
-            //int a = 10;
-            int b = 6;
-            //int b = 12;
-            //int sum = 1 + a + b;
-            //int sum = 2 + a + b;
-            int sum = 3 + a + b;
-            //int sum = 4 + a + b;
+// greeting – переводится как приветствие
+// var – нужен только при определении переменной
+            var greeting = "Father!";
+            System.out.println(greeting);
+            System.out.println(greeting);
 
-            System.out.println("sum = " + sum);
-
-
-    }
+        }
 }
 
 
