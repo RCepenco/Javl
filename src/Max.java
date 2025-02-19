e074bc7e22684097e1934e4f0239a53ae4a04c15
Rcepenko <rcepenkov@gmail.com>
2025-02-19 22:43:29 +0200

    Comment out a few lines to display "2 plus 3 is equal to 5" on the screen.

--- src/Max.java
+++ src/Max.java
@@ -1,11 +1,24 @@
 public class Max
-    {
+     {
         public static void main(String[] args)
         {
-            int a = 8, b = 3126;
-            String m = "My friend's birth year is";
-            System.out.println( m + " " + (b - a ));
+            int a = 3;
+            int b = 2;
 
-        }
+            //System.out.print("two");
+            System.out.print(b);
+            System.out.print(" plus ");
+            //System.out.print(" minus ");
+            System.out.print(a);
+            //System.out.print("three");
+            System.out.print(" is equal to ");
+            //System.out.print(" equals ");
+            //System.out.print("five");
+            System.out.print(a + b);
+
+
+
     }
+}
+
 
