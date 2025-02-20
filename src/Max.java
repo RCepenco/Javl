0cee08d932aa3d0839a65c443c973f7b7d92460b
Rcepenko <rcepenkov@gmail.com>
2025-02-20 10:00:31 +0200

    Write a Scanner with getWordForm

--- src/Max.java
+++ src/Max.java
@@ -1,17 +1,21 @@
 import java.util.Scanner;
-public class Max
-     {
-        public static void main(String[] args)
-        {
-            Scanner scanner = new Scanner(System.in);
-            int t = scanner.nextInt();  // если int (integer) вместо String то Input можно только цифры
+public class Max {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
 
-            System.out.println("Температура воздуха сегодня: " + t + " градусов.");
-            scanner.close();
-        }
-     }
+        String word = getWordForm(t);  /** используется для склонения "градусов" в зависимости от цифр в Input*/
+        System.out.println("Температура воздуха сегодня: " + t + " " + word);
+        scanner.close();
+    }
 
+    public static String getWordForm(int value) {
+        value = Math.abs(value) % 100;
+        if (value >= 11 && value <= 19) return "градусов";
 
-
-
-
+        value = value % 10;
+        if (value == 1) return "градус";
+        if (value >= 2 && value <= 4) return "градуса";
+        return "градусов";
+    }
+}
