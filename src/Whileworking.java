558062354cb375ea37e647b67387fca3562942b1
Rcepenko <rcepenkov@gmail.com>
2025-02-20 19:17:57 +0200

    While цикл

---
+++ src/Whileworking.java
@@ -0,0 +1,11 @@
+public class NextLevel {
+    public static void main(String[] args){
+        int value = 0;
+        while (value<5){ // цикл будет повторятся до 5 раз исходя из условия
+            System.out.println("Hello " + value);
+            value = value +1;
+        }
+
+    }
+
+}
