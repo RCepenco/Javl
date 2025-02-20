608807377feca1acb5e778b6c615a6386036eb3e
Rcepenko <rcepenkov@gmail.com>
2025-02-20 19:37:29 +0200

    While цикл

---
+++ src/ForLoop.java
@@ -0,0 +1,11 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {  // i++ == i = i+1
+            System.out.println("Hello " + i);
+
+
+        }
+    }
+}
+
+
--- src/Whileworking.java
+++ src/Whileworking.java
@@ -1,4 +1,4 @@
-public class NextLevel {
+public class Whileworking {
     public static void main(String[] args){
         int value = 0;
         while (value<5){ // цикл будет повторятся до 5 раз исходя из условия
