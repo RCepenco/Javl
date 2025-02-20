a3939938c61bac37a748e37e47dab5a808bac2ec
Rcepenko <rcepenkov@gmail.com>
2025-02-20 08:42:50 +0200

    Write programme Sample Input: Neo Sample Output: Hello, Neo

--- src/Max.java
+++ src/Max.java
@@ -1,10 +1,12 @@
+import java.util.Scanner;
 public class Max
      {
         public static void main(String[] args)
         {
-            var king = "King Balon the 6th";
-            var quantityRooms = 6 * 17;
-            System.out.println(king + " has " + quantityRooms + " rooms.");
+            Scanner scanner = new Scanner(System.in);
+            String name = scanner.nextLine();
+            System.out.println("Hello, " + name);
+            scanner.close();
 
 
         }
