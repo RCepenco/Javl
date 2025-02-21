b07cae2533e6c4ba88dd2654444accb94f88e43b
Rcepenko <rcepenkov@gmail.com>
2025-02-22 01:55:17 +0200

    Сложение1

--- src/MyNumber.java
+++ src/MyNumber.java
@@ -1,8 +1,9 @@
+import java.util.Scanner;
+
 class MyNumber {
     public static void main(String[] args) {
-        int valueX = 2;
-        int valueY = 4;
-        System.out.print(valueX + valueY);
-
+        Scanner inp = new Scanner(System.in);
+        System.out.println(inp.nextInt() + inp.nextInt());
+        inp.close();
     }
 }
